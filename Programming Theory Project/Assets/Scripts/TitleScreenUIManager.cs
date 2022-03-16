d89bb2e731c7967d1e47f9743fa074604b891afe
suboff <suboff@gmail.com>
2022-03-16 10:59:50 -0500

    Adding basic main menu UI

---
+++ Programming Theory Project/Assets/Scripts/TitleScreenUIManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreenUIManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("GameScreen");
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+}
